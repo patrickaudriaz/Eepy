7adbcbfa89f64aad77a70694a66e4d91762a8221
Patrick Audriaz (taaaupa3) <patrick.audriaz@swisscom.com>
2026-02-03 15:11:59 +0100

    Optimize animations and touch responsiveness

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -95,8 +95,14 @@ function selectBedTime() {
 }
 
 @keyframes float {
-  0% { transform: translateY(0px); }
-  50% { transform: translateY(-10px); }
-  100% { transform: translateY(0px); }
+  0% {
+    transform: translateY(0px);
+  }
+  50% {
+    transform: translateY(-10px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
 }
 </style>
